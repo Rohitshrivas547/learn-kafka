6296543552265c4b7c0f5bbc72f3e76d8c082530
Rohit Shrivas <rohitshrivas547@gmail.com>
2023-03-30 23:32:21 +0530

    first commit

---
+++ src/main/java/com/example/learnkafkaproject/Controller.java
@@ -0,0 +1,25 @@
+package com.example.learnkafkaproject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @Autowired
+    KafkaTemplate<String ,String> kafkaTemplate;
+
+    @Value("${myapp.topic_name}")
+    String topic;
+
+
+    @PostMapping("/produce")
+    public void produceMessage(@RequestParam("msg")String message){
+        kafkaTemplate.send(topic,message); //this kafka produce the message from java
+
+    }
+}
---
+++ src/test/java/com/example/learnkafkaproject/LearnKafkaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnkafkaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnKafkaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
